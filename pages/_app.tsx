f22efad1fadd25eda60c9dab1b77a8a46674a055
greatSumini <greatSumini@gmail.com>
2021-09-16 04:54:28 +0900

    feat: add meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import { ApolloProvider } from '@apollo/client';
@@ -14,6 +15,11 @@ export default function AdminApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <title>PICKK 카탈로그</title>
+        <meta name="author" content="PICKK 카탈로그" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <GlobalStyle />
       <ApolloProvider client={apolloClient}>
         <Component {...pageProps} />
